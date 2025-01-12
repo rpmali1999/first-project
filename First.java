0d5277561b279eefbebda21591fedb962bcb5b0b
rushikesh <rushikesh5mali@gmail.com>
2025-01-12 12:47:00 +0530

    first

---
+++ First.java
@@ -0,0 +1,17 @@
+package first;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i= 1;
+		if (i ==1 )
+		{
+			System.out.println("Welcome To Project");
+		}
+		
+
+	}
+
+}
